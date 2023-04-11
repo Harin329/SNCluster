3fe28de063f6a88e1563634f5ae36993e19f22d1
Jonathan <ho.ys.jonathan@gmail.com>
2023-04-10 18:10:07 -0700

    ensemble update

--- ensemble.py
+++ ensemble.py
@@ -1,17 +1,19 @@
 import pandas as pd
+import os.path
 
 geneTypes = ['nociceptionGenes', 'mechanoreceptionGenes', 'mechanoreception1Genes', 'mechanoreception2Genes', 'proprioceptionGenes']
-dataTags = ['hier', 'kmeans', 'leiden', 'wgcna']
+dataTags = ['hier', 'kmeans', 'leiden', 'wgcna', 'dbscan', 'hdb']
 
 for geneType in geneTypes:
     geneUnion = []
     for tag in dataTags:
-        data = pd.read_csv('data/' + tag + geneType + '.csv')
-        genes = data.iloc[:, 1].tolist()
-        if (geneUnion == []):
-            geneUnion = genes
-        else:
-            geneUnion = list(set(geneUnion) & set(genes))
+        if os.path.isfile('data/' + tag + geneType + '.csv'):
+            data = pd.read_csv('data/' + tag + geneType + '.csv')
+            genes = data.iloc[:, 1].tolist()
+            if (geneUnion == []):
+                geneUnion = genes
+            else:
+                geneUnion = set(geneUnion).intersection(set(genes))
 
-    df = pd.DataFrame(geneUnion)
+    df = pd.DataFrame(list(geneUnion))
     df.to_csv('data/' + geneType + '.csv', index=False, header=False)
